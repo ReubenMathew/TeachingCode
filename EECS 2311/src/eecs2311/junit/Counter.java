5e39a317cfd846ce556b8b0f1723e1ce7008d684
Reuben Mathew <intelphysic@gmail.com>
2020-02-03 10:21:28 -0500

    created 5 more sample test cases

--- EECS 2311/src/eecs2311/junit/Counter.java
+++ EECS 2311/src/eecs2311/junit/Counter.java
@@ -8,13 +8,28 @@ public class Counter {
         data = 0;
     }
     
+    public int reset() {
+    	data = 0;
+    	return this.data;
+    }
+    
     public int increment() {
         data++;
         return data;
     }
     
+    public int increment(int n) {
+        data += n;
+        return data;
+    }
+    
     public int decrement() {
         data--;
+        return data;
+    }
+    
+    public int decrement(int n) {
+        data -= n;
         return data;
     }
 }
--- EECS 2311/src/eecs2311/junit/CounterTest.java
+++ EECS 2311/src/eecs2311/junit/CounterTest.java
@@ -11,6 +11,12 @@ public class CounterTest {
     public void setUp() throws Exception {
         c = new Counter();
     }
+    
+    @Test
+    public void counterIntialization() {
+    	assertNotNull(c);
+    }
+
 
     @Test
     public void testIncrement() {
@@ -23,5 +29,30 @@ public class CounterTest {
         assertEquals(-1,c.decrement());
         assertEquals(-2,c.decrement());
     }
+    
+    @Test
+    public void testReset() {
+    	c.decrement();
+        assertEquals(0,c.reset());
+    }
+    
+    public void testIncrementArgs() {
+        assertEquals(4,c.increment(4));
+        assertEquals(6,c.increment(2));
+    }
 
+    @Test
+    public void testDecrementArgs() {
+        assertEquals(-4,c.decrement(4));
+        assertEquals(-6,c.decrement(2));
+    }
+    
+    @Test
+    public void testAllMethods() {
+        assertEquals(4,c.increment(4));
+        assertEquals(6,c.increment(2));
+        assertEquals(0,c.reset());
+        assertEquals(-4,c.decrement(4));
+        assertEquals(-6,c.decrement(2));
+    }
 }
